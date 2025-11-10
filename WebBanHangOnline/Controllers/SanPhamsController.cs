6edcad335fd6fe412550534135fb921c42128f4f
ducmanh <caoducmanh1611@gmail.com>
2025-11-10 21:25:15 +0700

    Tai len du an Web Ban Hang

---
+++ WebBanHangOnline/Controllers/SanPhamsController.cs
@@ -0,0 +1,172 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using WebBanHangOnline.Models;
+
+namespace WebBanHangOnline.Controllers
+{
+    public class SanPhamsController : Controller
+    {
+        private WebBanHangOnlineDBEntities db = new WebBanHangOnlineDBEntities();
+
+        ///Login
+        public ActionResult Login()
+        {
+            // Trả về View đăng nhập
+            return View();
+        }
+        [HttpPost]
+
+        public ActionResult Login(TaiKhoan tk)
+        {
+            var user = db.TaiKhoans.FirstOrDefault(u => u.Email == tk.Email && u.MatKhau == tk.MatKhau);
+            if (user != null)
+            {
+                Session["User"] = user;
+                return RedirectToAction("Index");
+            }
+            ViewBag.ErrorMessage = "Đăng nhập không thành công. Vui lòng kiểm tra lại thông tin.";
+            return View();
+        }
+
+
+        public ActionResult CreateLogin()
+        {
+             return View();
+        }
+
+       
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult CreateLogin(TaiKhoan tk)
+        {
+            if (ModelState.IsValid)
+            {
+                db.TaiKhoans.Add(tk);
+                db.SaveChanges();
+                return RedirectToAction("Login");
+            }
+            return View(tk);
+        }
+
+        // GET: SanPhams
+        public ActionResult Index()
+        {
+            var sanPhams = db.SanPhams.Include(s => s.DanhMuc);
+            return View(sanPhams.ToList());
+        }
+
+        // GET: SanPhams/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            SanPham sanPham = db.SanPhams.Find(id);
+            if (sanPham == null)
+            {
+                return HttpNotFound();
+            }
+            return View(sanPham);
+        }
+
+        // GET: SanPhams/Create
+        public ActionResult Create()
+        {
+            ViewBag.MaDanhMuc = new SelectList(db.DanhMucs, "MaDanhMuc", "TenDanhMuc");
+            return View();
+        }
+
+        // POST: SanPhams/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "MaSanPham,TenSanPham,SKU,MoTaNgan,MoTaChiTiet,GiaGoc,GiaKhuyenMai,SoLuongTon,HinhAnhChinh,LaHangMoi,LaBanChay,MaDanhMuc")] SanPham sanPham)
+        {
+            if (ModelState.IsValid)
+            {
+                db.SanPhams.Add(sanPham);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.MaDanhMuc = new SelectList(db.DanhMucs, "MaDanhMuc", "TenDanhMuc", sanPham.MaDanhMuc);
+            return View(sanPham);
+        }
+
+        // GET: SanPhams/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            SanPham sanPham = db.SanPhams.Find(id);
+            if (sanPham == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.MaDanhMuc = new SelectList(db.DanhMucs, "MaDanhMuc", "TenDanhMuc", sanPham.MaDanhMuc);
+            return View(sanPham);
+        }
+
+        // POST: SanPhams/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "MaSanPham,TenSanPham,SKU,MoTaNgan,MoTaChiTiet,GiaGoc,GiaKhuyenMai,SoLuongTon,HinhAnhChinh,LaHangMoi,LaBanChay,MaDanhMuc")] SanPham sanPham)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(sanPham).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.MaDanhMuc = new SelectList(db.DanhMucs, "MaDanhMuc", "TenDanhMuc", sanPham.MaDanhMuc);
+            return View(sanPham);
+        }
+
+        // GET: SanPhams/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            SanPham sanPham = db.SanPhams.Find(id);
+            if (sanPham == null)
+            {
+                return HttpNotFound();
+            }
+            return View(sanPham);
+        }
+
+        // POST: SanPhams/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            SanPham sanPham = db.SanPhams.Find(id);
+            db.SanPhams.Remove(sanPham);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
